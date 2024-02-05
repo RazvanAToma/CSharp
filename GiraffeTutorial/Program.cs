4e30e4cd2bd7d614903dec970a46dc6cd8af6884
Razvan Andrei TOMA <Razvan.a.toma@gmail.com>
2024-02-05 13:46:04 +0100

    skjdfhas

--- GiraffeTutorial/Program.cs
+++ GiraffeTutorial/Program.cs
@@ -1,7 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System;
 using System.Collections.Generic;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +12,15 @@ namespace GiraffeTutorial
 {
     class Program
     {
+        static void print(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+
         static void Main(string[] args)
         {
+            /*
             // Write to console
             Console.WriteLine("   /|");
             Console.WriteLine("  / |");
@@ -49,6 +58,61 @@ namespace GiraffeTutorial
             // Working With Strings
 
             Console.WriteLine("test");
+            */
+
+            // Oppgave 1
+            // a
+
+            /*
+            int num1 = 10;
+            int num2 = 10;
+
+            if (num1 > num2)
+            {
+                Console.WriteLine($"{num1} is larger than {num2}");
+            }
+            else if (num2 > num1)
+            {
+                Console.WriteLine($"{num2} is larger than {num1}");
+            }
+            else
+            {
+                Console.WriteLine("The numbers are the same.");
+            }
+            */
+
+
+            /*
+            var myCar = new Dictionary<string, string>()
+            {
+                {"Brand", "Ford"},
+                {"Model", "Explorer"},
+                {"Year", "2022"}
+            };
+
+            foreach (var i in myCar)
+            {
+                Console.WriteLine(i);
+            }
+            */
+
+            /*
+            List<string> myCars = new List<string>();
+
+            myCars.Add("Ford");
+            myCars.Add("Opel Zafira");
+            myCars.Add("Opel Astra");
+
+            int cars = 0;
+            foreach (string car in myCars)
+            {
+                Console.WriteLine($"Wow! You have a(n) {car}?");
+                cars += 1;
+            }
+            Console.WriteLine($"Wow! You have {cars} cars!?");
+            */
+
+            print("Hello");
         }
     }
 }
--- GiraffeTutorial/obj/Debug/net8.0/GiraffeTutorial.AssemblyInfo.cs
+++ GiraffeTutorial/obj/Debug/net8.0/GiraffeTutorial.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GiraffeTutorial")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abbac5fc5b4d8210035ed61d2c1486c49b990d82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01a9f6499e9a34077bfcff70d4c7673571d06213")]
 [assembly: System.Reflection.AssemblyProductAttribute("GiraffeTutorial")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GiraffeTutorial")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
