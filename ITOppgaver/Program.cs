bcd7340b6bfe630025f9519aa8814d8ae9dffdb0
Razvan Andrei TOMA <Razvan.a.toma@gmail.com>
2024-02-12 10:28:55 +0100

    blandede oppgaver funskjoner

--- ITOppgaver/Program.cs
+++ ITOppgaver/Program.cs
@@ -10,8 +10,9 @@ static double arealSirkel(int radius)
     return areal;
 }
 
-Console.WriteLine(arealSirkel(24));
-Console.WriteLine(arealSirkel(32));
+//Console.WriteLine(arealSirkel(24));
+//Console.WriteLine(arealSirkel(32));
+
 
 
 static double volumKule(int radius)
@@ -20,5 +21,73 @@ static double volumKule(int radius)
     return volum;
 }
 
-Console.WriteLine($"Volumet av kula er {volumKule(11)}");
-Console.WriteLine($"Volumet av kula er {volumKule(21)}");
+//Console.WriteLine($"Volumet av kula er {volumKule(11)}");
+//Console.WriteLine($"Volumet av kula er {volumKule(21)}");
+
+
+
+static string gammelhet(string navn, int alder)
+{
+    if (alder < 30)
+    {
+        return $"Hei {navn}, du er ung!";
+    }
+    else if (alder > 30)
+    {
+        return $"Hei {navn}, du er gammel!";
+    }
+    else
+    {
+        return $"Hei {navn}, du er midt i mellom...";
+    }
+}
+
+
+//Console.WriteLine("Hva heter du?");
+//string navn = Console.ReadLine();
+
+//Console.WriteLine("Hvor gammel er du?");
+//int alder = int.Parse(Console.ReadLine());
+
+//Console.WriteLine(gammelhet(navn, alder));
+
+
+
+static int kvadrat(int tall)
+{
+    return Convert.ToInt32(Math.Pow(tall, 2));
+}
+
+//int tall = 5;
+
+//Console.WriteLine($"Kvadratet av {tall} er {kvadrat(tall)}");
+
+
+
+static void tallSwap(int tall1, int tall2)
+{
+    int temp1 = tall1;
+    int temp2 = tall2;
+
+    int tall1s = temp2;
+    int tall2s = temp1;
+
+    Console.WriteLine(tall1s);
+    Console.WriteLine(tall2s);
+}
+
+//tallSwap(5, 10);
+
+
+
+static bool inRange(int tall, int minRange, int maxRange)
+{
+    if (tall > minRange && tall < maxRange)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+Console.WriteLine(inRange(7, 5, 10));
--- ITOppgaver/obj/Debug/net8.0/ITOppgaver.AssemblyInfo.cs
+++ ITOppgaver/obj/Debug/net8.0/ITOppgaver.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ITOppgaver")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a34020a26059ba03c1a7724e6e3e5b7da4f25cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a67921354f4675513412e474ddd8de951cb3f49")]
 [assembly: System.Reflection.AssemblyProductAttribute("ITOppgaver")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ITOppgaver")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
