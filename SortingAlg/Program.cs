96870ea8141547c3cbf6c553f55a5149937e160d
Razvan Andrei TOMA <Razvan.a.toma@gmail.com>
2024-03-07 10:42:34 +0100

    gay

---
+++ SortingAlg/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+
+
+Stopwatch sw = new Stopwatch();
+
+
+static Array createRandArr()
+{
+    Random random = new Random();
+
+    int[] randArray = new int[10];
+
+    int currentIndex = 0;
+
+    for (int i = 0; i < 10; i += 1)
+    {
+        randArray[currentIndex++] = random.Next(10);
+    }
+
+    return randArray;
+}
+
+Array randArray = createRandArr();
+
+foreach(int num in randArray)
+{
+    Console.WriteLine(num);
+}
+Console.WriteLine("-");
+
+//foreach(int number in randArray)
+//{
+//    int currentIndex = 0;
+
+//    if (number > randArray[currentIndex + 1])
+//    {
+//        Console.WriteLine($"{number} is bigger than {randArray[currentIndex + 1]}");
+//    }
+//}
+
+int index = 0;
+Console.WriteLine(randArray[index]);
